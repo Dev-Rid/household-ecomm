94a68e9a55158a175814d4eb4fc7006a3c03b6ab
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-01-29 16:16:27 +0100

    ...

--- src/componentss/home/index.jsx
+++ src/componentss/home/index.jsx
@@ -109,7 +109,7 @@ const Home = () => {
 
   return (
     <div className=''>
-        <div className='bg-primary-300 md:h-[85rem] lg:h-[88rem] border border-primary400'>
+        <div className='bg-primary-700 md:h-[85rem] lg:h-[88rem] border border-primary400'>
             <div className="pt-0">
                 <marquee behavior="" direction="" className="flex bg-primary-600 h-[7rem] pt-9">
                     <span className=' spa' data-aos="fade">Discount Sale + Free Delivery + Payment on delivery </span>
@@ -122,10 +122,10 @@ const Home = () => {
                 </marquee>
 
                 <div className='pt-12'>
-                    <h3 className='text-center text-3xl pb-8 sm:text-4xl md:text-6xl font-semibold text-primary-400 md:text-primary-400 shadow-lg' data-aos="fade">
+                    <h3 className='text-center text-3xl pb-8 sm:text-4xl md:text-6xl font-semibold text-primary-600 md:text-primary-600 shadow-lg' data-aos="fade">
                         New Collections
                     </h3>
-                    <p className=' pt-5 text-center text-gray-600 md:text-primary-400 text-md sm:text-xl md:text-3xl font-semilight' data-aos="fade">
+                    <p className=' pt-5 text-center text-primary-600 md:textprimary-600 text-md sm:text-xl md:text-3xl font-semilight' data-aos="fade">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis libero maiores ducimus dolores consequatur voluptatum repudiandae voluptatem vitae quis iusto?
                     </p>        
 
@@ -180,54 +180,54 @@ const Home = () => {
 
                 <div className="flex flex-col items-center justify-center min-h-[25rem] md:min-h-[32rem] lg:min-h-[35rem] md:pt-10 lg:pt-40" data-aos="fade-up">
                     <div>
-                        <video src="/src/img/demovid.mp4" className="shadow border-2 border-primary-400 rounded w-[15rem] md:w-[15rem] h-[15rem] md:h-[18rem]"></video>
+                        <video src="/src/img/demovid.mp4" className="shadow border-2 border-primary-600 rounded w-[15rem] md:w-[15rem] h-[15rem] md:h-[18rem]"></video>
                     </div>
                     <div className="text-center mt-4">
                         <Link to="target" smooth={true} duration={700}>
-                            <button className="rounded  p-3 md:p-2 text-sm md:text-lg text-gray-200 bg-primary-400 active:bg-primary-700 duration-300">
+                            <button className="rounded  p-3 md:p-2 text-sm md:text-lg text-gray-300 bg-primary-600 active:bg-yellow-800 duration-300">
                                 Yes! I want to order now
                             </button>
                         </Link> 
-                        <p className="mt-2 text-gray-600" data-aos="slide-up">Work smart</p>
+                        <p className="mt-2 text-primary-600" data-aos="slide-up">Work smart</p>
                     </div>
                 </div>
 
             </div>
         </div>
 
-        <div className='bg-primary-300 min-h-[200rem] pt-9 md:pt-10'>
+        <div className='bg-primary-700 min-h-[200rem] pt-9 md:pt-10'>
             <div className="flex justify-center mt-0 md:mt-32 w[50rem]  min-h[30rem] md:min-h[40rem] lg:min-h[45rem] " data-aos="fade-up">
-                <img src="/src/img/demo.jpg" alt="bigimg" className='h-[15rem] md:h-[22rem] lg:h-[25rem] w-[17rem] md:w-[35rem] lg:w-[40rem] border-2 border-primary-400' />
+                <img src="/src/img/demo.jpg" alt="bigimg" className='h-[15rem] md:h-[22rem] lg:h-[25rem] w-[17rem] md:w-[35rem] lg:w-[40rem] border-2 border-primary-600' />
             </div>
 
             <div className='m-12 text-left -pl-[1rem] md:pl-[2rem] lg:pl-[8rem] lg:pl58 text-primary-400' data-aos="slide-right">
-                <h4 className='pl-1 font-bold text-md md:text-2xl text-yellow-600'>Reasons why you will love this product</h4>
-                <p className='pt-4 text-sm md:text-lg text-gray-700'>1. Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                <p className='text-sm md:text-lg text-gray-700'>2. Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <h4 className='pl-1 font-bold text-md md:text-2xl text-yellow-700'>Reasons why you will love this product</h4>
+                <p className='pt-4 text-sm md:text-lg text-primary-600'>1. Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <p className='text-sm md:text-lg text-primary-600'>2. Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             </div>
 
 
             <div className='flex flex-col justify-center items-center min-h-[30rem] md:min-h-[40rem] pt-[1rem] md:pt-[1rem] lg:pt-[1rem]' data-aos="fade-up">
                 <div>
-                    <img src="/src/img/demo.jpg" alt="bigimg" className='border-2 border-primary-400 rounded w-[12rem] md:w-[15rem] h-[15rem] md:h-[18rem]' />
+                    <img src="/src/img/demo.jpg" alt="bigimg" className='border-2 border-primary-600 rounded w-[12rem] md:w-[15rem] h-[15rem] md:h-[18rem]' />
                 </div>
                 <Link to='targetform' smooth={true} duration={800}>
-                    <button className='rounded p-1 md:p-2 mt-5 text-lg md:text-lg text-gray-100 bg-primary-400 active:bg-yellow-700 duration-200'>Yes, I want to order now</button>
+                    <button className='rounded p-1 md:p-2 mt-5 text-lg md:text-lg text-gray-300 bg-primary-600 active:bg-yellow-700 duration-200'>Yes, I want to order now</button>
                 </Link>
             </div>
 
       
-            <div className='border-2 border-primary-400 rounded-xl m-auto mt-12 w-[17rem] sm:w-[25rem] md:w-[35rem] lg:w-[50rem] h-[40rem] md:h-[44rem] md:min-h-dvh' data-aos="fade-up"> 
-                <div className='h-[6rem] bg-primary-400 rounded-t-md'>
+            <div className='border-2 border-primary-600 rounded-xl m-auto mt-12 w-[17rem] sm:w-[25rem] md:w-[35rem] lg:w-[50rem] h-[40rem] md:h-[44rem] md:min-h-dvh' data-aos="fade-up"> 
+                <div className='h-[6rem] bg-primary-600 rounded-t-md'>
                     <h6 className='text-center py-10 md:py-8 md:p-5 text-xl sm:text-2xl md:text-4xl font-bold text-gray-300'>Massive Discount Today</h6>
                 </div>
 
-                <div className='h-[6rem] md:h-[8rem] bg-primary-300 border-b border-primary-400'>
-                    <h3 className='text-center p-4 font-semibold text-lg md:text-2xl text-primary-400'>Free Delivery</h3>
-                    <h3 className='text-center font-semibold text-lg md:text-2xl text-primary-400'>Payment on Delivery</h3>
+                <div className='h-[6rem] md:h-[8rem] bg-primary-700 border-b border-primary-600'>
+                    <h3 className='text-center p-4 font-semibold text-lg md:text-2xl text-primary-600'>Free Delivery</h3>
+                    <h3 className='text-center font-semibold text-lg md:text-2xl text-primary-600'>Payment on Delivery</h3>
                 </div>
                 
-                <div className='text-center border-b border-primary-400 h-[13rem] md:h-[15rem] pt-6'>
+                <div className='text-center border-b border-primary-600 h-[13rem] md:h-[15rem] pt-6'>
                     <h4 className='text-xl text-gray-800 md:text-3xl font-bold underline'>1 Set</h4>
                     <h4 className='font-semilight text-lg md:text-2xl'>Original Price <span className='text-primary-500 font-bold text-3xl md:text-4xl ml-2 md:ml-6'>00000</span></h4>
                     <h4 className='font-semibold sm:font-bold text-gray-700 text-xl md:text-3xl'>✅Discount Price <span className='text-green-600 font-bold text-3xl md:text-4xl ml-2 md:ml-6'>00000</span></h4>
@@ -246,14 +246,14 @@ const Home = () => {
 
                 <div className='text-center p-10'>
                     <Link to='target' smooth={true} duration={700}>
-                        <button className='rounded px-1 md:px-7 py-1 md:py-3 text-xl  text-primary-400 active:text-primary-300 bg-primary400 active:bg-primary-400 border-2 border-primary-400 duration-300' >YES, I WANT THIS NOW</button>
+                        <button className='rounded px-1 md:px-7 py-1 md:py-3 text-xl text-primary-600 active:textprimary-700 active:bgprimary-600 border-2 border-primary-600 duration-300' >YES, I WANT THIS NOW</button>
                     </Link>
-                    <p className='text-primary-400 text-xl md:text-2xl font-bold pt-2'>This Discount Ends Today…</p>
+                    <p className='text-primary-600 text-xl md:text-2xl font-bold pt-2'>This Discount Ends Today…</p>
                 </div>
             </div>
 
-            <div id='target' className='min-h-[60rem] m-auto shadow-2xl mt-[18rem] md:mt-[20rem] lg:mt-[25rem] w-[18rem] sm:w-[30rem] md:w-[35rem] border-2 border-primary-400 rounded flex-row justify-center'>
-                <div className='text-center bg-primary-400 shadow-2xl'>
+            <div id='target' className='min-h-[60rem] m-auto shadow-2xl mt-[18rem] md:mt-[20rem] lg:mt-[25rem] w-[18rem] sm:w-[30rem] md:w-[35rem] border-2 border-primary-600 rounded flex-row justify-center'>
+                <div className='text-center bg-primary-600 shadow-2xl'>
                     <h4 className='text-gray-400 text-xl md:text-2xl font-bold pt-4'>HURRY DISCOUNT EXPIRES TODAY</h4>
                     <p className='text-lg md:text-2xl border-b border-primary-300 font-bold text-gray-400 pt-2'>Fill Order Form Below</p>
                     <p className='text-center text-gray-400 text-sm sm:text-md md:text-xl pt-2 p-4'><span className='text-yellow-500 font-bold p-3'>
@@ -262,10 +262,10 @@ const Home = () => {
                 {/* onSubmit={handleSubmit} */}
                 <form action="" className='p-10' id='targetform' onSubmit={handleSubmit}>
                     <div className='flex flex-col myInputLabel'>
-                        <label htmlFor="" className='text-md md:text-xl font-semibold text-gray-700'>Your Name <span className='text-red-600'>*</span></label>
+                        <label htmlFor="" className='text-md md:text-xl font-semibold text-primary-600'>Your Name <span className='text-red-600'>*</span></label>
                         <input 
                             type="text" 
-                            className='md:w-[30rem] pl-4 text-xl rounded h-[3rem] bg-primary-300 border-4 text-gray-700 font-semibold border-gray-400'
+                            className='md:w-[30rem] pl-4 text-xl rounded h-[3rem] bg-primary-700 border-4 text-primary-600 font-semibold border-primary-600'
                             name="name"
                             onChange={handleChange}
                             required
@@ -273,10 +273,10 @@ const Home = () => {
                     </div>
                     
                     <div className='pt-7 flex flex-col myInputLabel'>
-                        <label htmlFor="" className='text-md md:text-xl font-semibold text-gray-700'>Your Phone / Whatsapp Number <span className='text-red-600'>*</span></label>
+                        <label htmlFor="" className='text-md md:text-xl font-semibold text-primary-600'>Your Phone / Whatsapp Number <span className='text-red-600'>*</span></label>
                         <input 
                             type="number" 
-                            className='no-spinner md:w-[22rem] pl-4 text-xl rounded h-[3rem] bg-primary-300 border-4 text-gray-700 font-semibold border-gray-400'
+                            className='no-spinner md:w-[22rem] pl-4 text-xl rounded h-[3rem] bg-primary-700 border-2 text-primary-600 font-semibold border-primary-600'
                             name="phoneNumber"
                             onChange={handleChange}
                             required
@@ -284,10 +284,10 @@ const Home = () => {
                     </div>
                     
                     <div className='pt-7 flex flex-col myInputLabel'>
-                        <label htmlFor="" className='text-md md:text-xl font-semibold text-gray-700'>Second Number <span className='text-red-600'>*</span></label>
+                        <label htmlFor="" className='text-md md:text-xl font-semibold text-primary-600'>Second Number <span className='text-red-600'>*</span></label>
                         <input 
                             type="number" 
-                            className='no-spinner md:w-[22rem] pl-4 text-xl rounded h-[3rem] bg-primary-300 border-4 text-gray-700 font-semibold border-gray-400'
+                            className='no-spinner md:w-[22rem] pl-4 text-xl rounded h-[3rem] bg-primary-700 border-4 text-primary-600 font-semibold border-primary-600'
                             name="secondNumber"
                             onChange={handleChange}
                             required
@@ -295,10 +295,10 @@ const Home = () => {
                     </div>
                     
                     <div className='pt-7 flex flex-col myInputLabel'>
-                        <label htmlFor="" className='text-md md:text-xl font-semibold text-gray-700'>Your State <span className='text-red-600'>*</span></label>
+                        <label htmlFor="" className='text-md md:text-xl font-semibold text-primary-600'>Your State <span className='text-red-600'>*</span></label>
                         <input 
                             type="text" 
-                            className='md:w-[22rem] pl-4 text-xl rounded h-[3rem]  bg-primary-300 border-4 text-gray-700 font-semibold border-gray-400'
+                            className='md:w-[22rem] pl-4 text-xl rounded h-[3rem]  bg-primary-700 border-4 text-primary-600 font-semibold border-primary-600'
                             name="yourState"
                             onChange={handleChange}
                             required
@@ -306,10 +306,10 @@ const Home = () => {
                     </div>
                     
                     <div className='pt-7 flex flex-col myInputLabel'>
-                        <label htmlFor="" className='text-md md:text-xl font-semibold text-gray-700'>Your Home/Office Address (Delivery Address) <span className='text-red-600'>*</span></label>
+                        <label htmlFor="" className='text-md md:text-xl font-semibold text-primary-600'>Your Home/Office Address (Delivery Address) <span className='text-red-600'>*</span></label>
                         <input 
                             type="text" 
-                            className='  md:w-[30rem] p-1 sm:pl-3 md:pl-4 text-md md:text-xl rounded h-[3rem] text-gray-700 font-semibold bg-primary-300 border-4 border-gray-400' 
+                            className='  md:w-[30rem] p-1 sm:pl-3 md:pl-4 text-md md:text-xl rounded h-[3rem] text-primary-600 font-semibold bg-primary-700 border-4 border-primary-600' 
                             name='deliveryAddress'
                             placeholder='Please enter a detailed address'
                             onChange={handleChange}
@@ -345,7 +345,7 @@ const Home = () => {
                        </ul>
                     </div>
                     <button 
-                        className='w-full text-center border-4 font-bold border-primary-400 mt-10 py-3 px-2 rounded-lg  text-primary-400 active:text-primary-300 text-md md:text-xl active:bg-yellow-700 active:border-4 active:border-primary-300 activeborder '
+                        className='w-full text-center border-4 font-bold border-primary-600 mt-10 py-3 px-2 rounded-lg  text-primary-600 active:text-primary-300 text-md md:text-xl active:bg-primary-700 active:border-4 active:border-primary-300 activeborder '
                         // onClick={handleClick}    
                     >Submit Your Order</button>
                 </form>
@@ -362,8 +362,8 @@ const Home = () => {
 
             
             <div className='flex-row justify-center pt-14'>
-                <h3 className='font-bold text-xl md:text-2xl text-center text-gray-600'>DISCLAIMER</h3>
-                <p className='leading6 p-2 font-semibold text-center text-gray-500 w[50rem] text-sm md:text-xl'>This site is not a part of the Facebook website, Facebook Inc. 
+                <h3 className='font-bold text-xl md:text-2xl text-center text-primary-600'>DISCLAIMER</h3>
+                <p className='leading6 p-2 font-semibold text-center text-gray-700 w[50rem] text-sm md:text-xl'>This site is not a part of the Facebook website, Facebook Inc. 
                     It is also not part of the Google Website or Alphabet Inc. 
                     Additionally, this site is not endorsed by Facebook or Google in any way.
                     FACEBOOK and GOOGLE is a trademark of FACEBOOK Inc and ALPHABET INC respectively.
